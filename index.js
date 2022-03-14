1a997c05e52834b61d72868354718be74076298e
Paul Bond <git@paulsbond.co.uk>
2022-03-14 16:45:48 +0000

    Sort of

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-var origin = [480, 300]
-var j = 10
-var scale = 20
 var scatter = []
-var yLine = []
-var beta = 0
-var alpha = 0
 var key = function (d) { return d.id; }
 var startAngle = Math.PI / 4;
 var svg = d3.select('svg')
@@ -20,12 +14,12 @@ var point3d = d3._3d()
     .x(function (d) { return d.x; })
     .y(function (d) { return d.y; })
     .z(function (d) { return d.z; })
-    .origin(origin)
+    .origin([250, 250])
     .rotateY(startAngle)
     .rotateX(-startAngle)
-    .scale(scale);
+    .scale(20);
 
-function processData(data, tt) {
+function processData(data) {
 
     var points = svg.selectAll('circle').data(data[0], key);
 
@@ -37,11 +31,10 @@ function processData(data, tt) {
         .attr('cx', posPointX)
         .attr('cy', posPointY)
         .merge(points)
-        .transition().duration(tt)
-        .attr('r', 3)
-        .attr('stroke', function (d) { return d3.color(color(d.id)).darker(3); })
-        .attr('fill', function (d) { return color(d.id); })
-        .attr('opacity', 1)
+        .attr('r', function (d) { return d.r; })
+        .attr('stroke', function (d) { 'black' })
+        .attr('fill', function (d) { 'black' })
+        .attr('opacity', function (d) { return d.opacity; })
         .attr('cx', posPointX)
         .attr('cy', posPointY);
 
@@ -61,16 +54,21 @@ function posPointY(d) {
 function init() {
     var cnt = 0;
     scatter = [];
-    for (var z = -j; z < j; z++) {
-        for (var x = -j; x < j; x++) {
-            scatter.push({ x: x, y: d3.randomUniform(0, -10)(), z: z, id: 'point_' + cnt++ });
+    scatter.push({ x: 0, y: 0, z: 0, r: 100, opacity: 0.3, id: 'point_' + cnt++ });
+    for (var x = -5; x <= 5; x++) {
+        for (var y = -5; y <= 5; y++) {
+            for (var z = -5; z <= 5; z++) {
+                var dist = Math.sqrt(x * x + y * y + z * z);
+                var opacity = (dist > 4.9 & dist < 5.1) ? 1.0 : 0.1;
+                scatter.push({ x: x, y: y, z: z, r: 1, opacity: opacity, id: 'point_' + cnt++ });
+            }
         }
     }
 
     var data = [
         point3d(scatter),
     ];
-    processData(data, 1000);
+    processData(data);
 }
 
 function dragStart() {
@@ -81,19 +79,17 @@ function dragStart() {
 function dragged() {
     mouseX = mouseX || 0;
     mouseY = mouseY || 0;
-    beta = (d3.event.x - mx + mouseX) * Math.PI / 230;
-    alpha = (d3.event.y - my + mouseY) * Math.PI / 230 * (-1);
+    var beta = (d3.event.x - mx + mouseX) * Math.PI / 230;
+    var alpha = (d3.event.y - my + mouseY) * Math.PI / 230 * (-1);
     var data = [
         point3d.rotateY(beta + startAngle).rotateX(alpha - startAngle)(scatter),
     ];
-    processData(data, 0);
+    processData(data);
 }
 
 function dragEnd() {
     mouseX = d3.event.x - mx + mouseX;
     mouseY = d3.event.y - my + mouseY;
 }
-
-d3.selectAll('button').on('click', init);
 
 init();
